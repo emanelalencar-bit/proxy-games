ec075b3ee4a886cef5aa4b95f1d2c57f7cf32413
emanelalencar-bit <emanelalencar@gmail.com>
2026-04-08 10:45:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const express = require("express");
-const axios = require("axios");
-
-const app = express();
-
-// Página inicial
-app.get("/", (req, res) => {
-  res.send("Proxy funcionando!");
-});
-
-// Rota para descobrir o IP da VPS
 app.get("/ip", async (req, res) => {
+  const axios = require("axios");
   try {
     const response = await axios.get("https://api.ipify.org?format=json");
-    res.send(`IP da VPS: ${response.data.ip}`);
-  } catch (error) {
+    res.send(response.data.ip);
+  } catch (err) {
     res.send("Erro ao pegar IP");
   }
-});
-
-// iniciar servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
 });
